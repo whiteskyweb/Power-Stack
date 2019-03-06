4eb7796c69ecba0396eb59b837095d84ff58847a
Xavier Mirabelli-Montan <xavier@travelnation.co.uk>
2019-03-06 16:40:22 +0000

    Adding a separate release step

--- start.sh
+++ start.sh
@@ -23,7 +23,8 @@ if hash docker 2>/dev/null; then
   printf "\n${GREEN_BG}Starting your development containers...${NC}\n\n"
 
   set +x
-    docker-compose -f ./infrastructure/docker-compose/docker-compose.yml -f ./infrastructure/docker-compose/docker-compose-dev.yml up -d
+
+  docker-compose -f ./infrastructure/docker-compose/docker-compose.yml -f ./infrastructure/docker-compose/docker-compose-dev.yml up -d
 
   printf "\n${GREEN_BG}Done${NC}\n\n"
 
--- stop.sh
+++ stop.sh
@@ -7,7 +7,7 @@ if hash docker 2>/dev/null; then
 
   printf "\n${GREEN_BG}Stopping your development containers...${NC}\n\n"
 
-  set +x
+    set +x
     docker-compose -f ./infrastructure/docker-compose/docker-compose.yml -f ./infrastructure/docker-compose/docker-compose-dev.yml down
 
   printf "\n${GREEN_BG}Done${NC}\n\n"
